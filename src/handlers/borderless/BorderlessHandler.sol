6f0ccc1a83010591eb4bd7040298e3af1305a0fd
Aercwarden <arc@dopex.io>
2025-07-30 20:51:10 +0400

    feat: bordleress handler progress

---
+++ src/handlers/borderless/BorderlessHandler.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0 <0.9.0;
+
+import {V3BaseHandlerKodiak} from "../V3BaseHandlerKodiak.sol";
+import {LiquidityManager} from "./LiquidityManager.sol";
+
+import {IBubblySwapPool as IV3Pool} from "./IBubblySwapPool.sol";
+
+/// @title UniswapV3Handler
+/// @author arcwardeth
+/// @notice Handles Borderless specific operations
+/// @dev Inherits from V3BaseHandlerKodiak and LiquidityManager
+contract BorderlessHandler is V3BaseHandlerKodiak, LiquidityManager {
+    /// @notice Constructs the UniswapV3Handler contract
+    /// @param _owner Address of the contract owner
+    /// @param _feeReceiver Address to receive fees
+    /// @param _factory Address of the Borderless factory
+    constructor(address _owner, address _feeReceiver, address _factory)
+        V3BaseHandlerKodiak(_owner, _feeReceiver)
+        LiquidityManager(_factory)
+    {}
+
+    /// @notice Adds liquidity to a Borderless pool
+    /// @dev Overrides the _addLiquidity function from V3BaseHandler
+    /// @param self Whether the function is called internally or externally
+    /// @param tki TokenIdInfo struct containing token and fee information
+    /// @param tickLower The lower tick of the position
+    /// @param tickUpper The upper tick of the position
+    /// @param amount0 The amount of token0 to add as liquidity
+    /// @param amount1 The amount of token1 to add as liquidity
+    /// @return l The amount of liquidity added
+    /// @return a0 The actual amount of token0 added as liquidity
+    /// @return a1 The actual amount of token1 added as liquidity
+    function _addLiquidity(
+        bool self,
+        TokenIdInfo memory tki,
+        int24 tickLower,
+        int24 tickUpper,
+        uint256 amount0,
+        uint256 amount1
+    ) internal virtual override returns (uint128 l, uint256 a0, uint256 a1) {
+        if (!self) {
+            (l, a0, a1,) = addLiquidity(
+                LiquidityManager.AddLiquidityParams({
+                    token0: tki.token0,
+                    token1: tki.token1,
+                    fee: tki.fee,
+                    recipient: address(this),
+                    tickLower: tickLower,
+                    tickUpper: tickUpper,
+                    amount0Desired: amount0,
+                    amount1Desired: amount1,
+                    amount0Min: amount0,
+                    amount1Min: amount1
+                })
+            );
+        } else {
+            (l, a0, a1,) = BorderlessHandler(address(this)).addLiquidity(
+                LiquidityManager.AddLiquidityParams({
+                    token0: tki.token0,
+                    token1: tki.token1,
+                    fee: tki.fee,
+                    recipient: address(this),
+                    tickLower: tickLower,
+                    tickUpper: tickUpper,
+                    amount0Desired: amount0,
+                    amount1Desired: amount1,
+                    amount0Min: amount0,
+                    amount1Min: amount1
+                })
+            );
+        }
+    }
+
+    /// @notice Removes liquidity from a Borderless pool
+    /// @dev Overrides the _removeLiquidity function from V3BaseHandler
+    /// @param _pool The Borderless pool
+    /// @param tickLower The lower tick of the position
+    /// @param tickUpper The upper tick of the position
+    /// @param liquidity The amount of liquidity to remove
+    /// @return amount0 The amount of token0 removed
+    /// @return amount1 The amount of token1 removed
+    function _removeLiquidity(IV3Pool _pool, int24 tickLower, int24 tickUpper, uint128 liquidity)
+        internal
+        virtual
+        override
+        returns (uint256 amount0, uint256 amount1)
+    {
+        (amount0, amount1) = _pool.burn(tickLower, tickUpper, liquidity);
+    }
+}
